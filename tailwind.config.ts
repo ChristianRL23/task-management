067ed76684abb035eb9776914c7c14f9d759411e
Christian R. Lara <christianramirezlara1@gmail.com>
2024-05-22 11:17:29 -0600

    Prepare project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      white: '#FFFFFF',
+      blue: '#635FC7',
+      purple: '#A8A4FF',
+      black: '#000112',
+      'black-2': '#20212C',
+      'black-3': '#2B2C37',
+      'black-4': '#3E3F4E',
+      gray: '#828FA3',
+      'sky-blue': '#E4EBFA',
+      'sky-blue-light': '#F4F7FD',
+      red: '#EA5555',
+      'red-light': '#FF9898',
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
       },
     },
   },

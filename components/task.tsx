79cae5a7e8a9edcc52292086e324a2a33ab67f6c
Christian R. Lara <christianramirezlara1@gmail.com>
2024-05-22 17:26:41 -0600

    Create Task component

---
+++ components/task.tsx
@@ -0,0 +1,10 @@
+const Task = () => {
+  return (
+    <div className="py-7 px-5 shadow rounded-xl w-96">
+      <h4 className="font-bold text-2xl tracking-tighter mb-2">Task name</h4>
+      <h6 className="font-bold text-gray">0 of 3 substasks</h6>
+    </div>
+  );
+};
+
+export default Task;
